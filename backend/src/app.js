dabebdf7091fab12d5f651c52e80472a8ddb7940
Ritik Raj <149468843+ritik5504@users.noreply.github.com>
2026-05-19 23:44:12 +0530

    chnage some url

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ import cors from "cors";
 const app=express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://dev-connect-liard.vercel.app",
   credentials: true,
 }));
 
